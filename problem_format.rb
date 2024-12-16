053832bf008bc7d72bb490da245511dc2bb93ffc
Grant Davis <grantdavis303@gmail.com>
2024-12-16 16:05:42 -0700

    Add Problem Format

---
+++ problem_format.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+# Problem #
+
+# Description
+
+# Input:
+# Output:
+
+def method(input)
+  binding.pry
+
+  # Set variables
+  # Return
+end
+
+p method('input')
+# Expecting
+
+p method('input')
+# Expecting
+
+p method('input')
+# Expecting
+
+p method('input')
+# Expecting
