694e83352c54a75ebf49acdc39b8a0ed81ed45ae
Grant Davis <grantdavis303@gmail.com>
2024-12-23 13:19:35 -0700

    Update Problem Format

--- problem_format.rb
+++ problem_format.rb
@@ -5,7 +5,7 @@ require 'pry'
 # 1. Read the problem or prompt thoroughly
 # 2. Define input and expected output
 # 3. Determine constraints and create tests (with expected outcomes)
-# 4. Brainstorm ideas
+# 4. Brainstorm ideas for solution that satisfy all tests
 # 5. Build the method/function
 
 # Problem #
